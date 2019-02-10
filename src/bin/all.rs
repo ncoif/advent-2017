42cb8fa93319640bdb382abeb1f9d839fc21ca9d
ncoif <github@ncoif.net>
2019-02-10 18:50:05 +0000

    day01 answer2

--- src/bin/all.rs
+++ src/bin/all.rs
@@ -5,7 +5,10 @@ fn run_day(day: u32) {
     let input = std::fs::read_to_string(filename).unwrap();
 
     let answer1 = day01::answer1(&input);
-    println!("Day {:02}: Inverse Captcha ({}/2): {}", day, 1, answer1)
+    println!("Day {:02}: Inverse Captcha (1/2): {}", day, answer1);
+
+    let answer2 = day01::answer2(&input);
+    println!("Day {:02}: Inverse Captcha (2/2): {}", day, answer2);
 }
 
 fn main() {
--- src/day01.rs
+++ src/day01.rs
@@ -1,17 +1,31 @@
-use itertools::zip;
-
 pub fn answer1(input: &str) -> u32 {
     let digits: Vec<u32> = input
         .chars()
         .map(|c| c.to_digit(10))
         .filter(Option::is_some)
-        .map(Option::unwrap)
+        .map(Option::unwrap) // safe because of the .is_some()
         .collect();
 
-    let mut digits_cycled: Vec<u32> = digits.iter().skip(1).cloned().collect();
-    digits_cycled.push(digits[0]);
+    let digits_cycled_iter = digits.iter().cycle().skip(1);
+    digits
+        .iter()
+        .zip(digits_cycled_iter)
+        .fold(0, |sum, (a, b)| if a == b { sum + a } else { sum })
+}
 
-    zip(&digits, &digits_cycled).fold(0, |sum, (a, b)| if a == b { sum + a } else { sum })
+pub fn answer2(input: &str) -> u32 {
+    let digits: Vec<u32> = input
+        .chars()
+        .map(|c| c.to_digit(10))
+        .filter(Option::is_some)
+        .map(Option::unwrap) // safe because of the .is_some()
+        .collect();
+
+    let digits_cycled_iter = digits.iter().cycle().skip(digits.len() / 2);
+    digits
+        .iter()
+        .zip(digits_cycled_iter)
+        .fold(0, |sum, (a, b)| if a == b { sum + a } else { sum })
 }
 
 #[test]
@@ -32,4 +46,29 @@ fn test_answer1_1234() {
 #[test]
 fn test_answer1_91212129() {
     assert_eq!(answer1("91212129"), 9);
+}
+
+#[test]
+fn test_answer2_1212() {
+    assert_eq!(answer2("1212"), 6);
+}
+
+#[test]
+fn test_answer2_1221() {
+    assert_eq!(answer2("1221"), 0);
+}
+
+#[test]
+fn test_answer2_123425() {
+    assert_eq!(answer2("123425"), 4);
+}
+
+#[test]
+fn test_answer2_123123() {
+    assert_eq!(answer2("123123"), 12);
+}
+
+#[test]
+fn test_answer2_12131415() {
+    assert_eq!(answer2("12131415"), 4);
 }
