56ffba0795fc17563c7d2e172654c5d8b18506ba
ncoif <github@ncoif.net>
2019-02-14 13:00:06 +0000

    wip day07: delimiter for parser

--- src/day07.rs
+++ src/day07.rs
@@ -23,7 +23,10 @@ named!(
 named!(
     weight_parser<u32>,
     map_res!(
-        map_res!(nom::digit, std::str::from_utf8),
+        map_res!(
+            delimited!(char!('('), is_not!(")"), char!(')')),
+            std::str::from_utf8
+        ),
         std::str::FromStr::from_str
     )
 );
@@ -37,9 +40,8 @@ named!(
     line_parser<Line>,
     do_parse!(
         name: name_parser
-            >> tag!(" (")
+            >> tag!(" ")
             >> weight: weight_parser
-            >> tag!(")")
             >> opt!(tag!(" -> "))
             >> children: opt!(children_parser)
             >> (Line {
@@ -83,6 +85,12 @@ fn parse_input(input: &str) -> Graph<(&str, u32), (&str, u32)> {
 fn parse_simple_name() {
     assert_eq!(name_parser(b"pbga "), Ok((&b" "[..], "pbga".to_string())));
     assert_eq!(name_parser(b"pbga\n"), Ok((&b"\n"[..], "pbga".to_string())));
+}
+
+#[test]
+fn parse_simple_weight() {
+    assert_eq!(weight_parser(b"(66) "), Ok((&b" "[..], 66)));
+    assert_eq!(weight_parser(b"(66)\n"), Ok((&b"\n"[..], 66)));
 }
 
 #[test]
