4877845951644ddbf59c8aef943dd0195287079f
ncoif <github@ncoif.net>
2019-02-16 22:21:37 +0000

    day07 answer2

--- src/day07.rs
+++ src/day07.rs
@@ -45,13 +45,49 @@ named!(
     )
 );
 
-struct GraphForRecursion {
-    graph: Graph<(String, u32), u32>,
+// (String, u32, u32) -> (name, weight, children weight)
+#[derive(Debug, Clone, PartialEq)]
+struct Node(String, u32, u32);
+
+#[derive(Debug)]
+struct Towers {
+    graph: Graph<Node, u32>,
 }
 
-impl GraphForRecursion {
+impl Towers {
+    fn new(lines: &[Line]) -> Towers {
+        let mut graph = Graph::<Node, u32>::new();
+
+        // add all the nodes
+        let mut nodes: HashMap<String, NodeIndex> = HashMap::new();
+        for line in lines {
+            let name = &line.name;
+            let node = graph.add_node(Node(name.clone(), line.weight, 0));
+            nodes.insert(name.clone(), node);
+        }
+
+        // add all the edges
+        for line in lines {
+            if line.children.is_some() {
+                for child in &line.children.clone().unwrap() {
+                    graph.update_edge(nodes[&line.name], nodes[child], 0); //0: no edge weight
+                }
+            }
+        }
+
+        Towers { graph }
+    }
+
+    fn get(&self, idx: NodeIndex) -> Node {
+        self.graph[idx].clone()
+    }
+
     fn leafs(&self) -> Vec<NodeIndex> {
         self.graph.externals(Direction::Outgoing).collect()
+    }
+
+    fn source(&self) -> NodeIndex {
+        self.graph.externals(Direction::Incoming).next().unwrap()
     }
 
     fn parent(&self, node: NodeIndex) -> Option<NodeIndex> {
@@ -68,54 +104,23 @@ impl GraphForRecursion {
     fn children_weight(&self, node: NodeIndex) -> u32 {
         self.children(node)
             .iter()
-            .map(|idx| self.graph[*idx].1)
+            .map(|idx| {
+                let child = self.get(*idx);
+                child.1 + child.2
+            })
             .sum()
-    }
-
-    // return the NodeIndex of the unbalanced child, as well as the invalid_weight and expected weight
-    fn children_weigh_unbalanced(&self, node: NodeIndex) -> Option<(NodeIndex, u32, u32)> {
-        let children = self.children(node);
-        if children.is_empty() {
-            None
-        } else {
-            let mut weight_frequency: HashMap<u32, usize> = HashMap::new();
-            for child in &children {
-                let child_weight = self.graph[*child].1;
-                weight_frequency
-                    .entry(child_weight)
-                    .and_modify(|f| *f += 1)
-                    .or_insert(1);
-            }
-
-            if weight_frequency.len() == 1 {
-                None
-            } else {
-                println!("weight_frequency {:?}", weight_frequency);
-                let (expected_weight, _) = weight_frequency.iter().find(|(_w, f)| **f > 1).unwrap();
-                let (invalid_weight, _) = weight_frequency.iter().find(|(_w, f)| **f == 1).unwrap();
-                let mut unbalanced_node = children[0];
-                for child in &children {
-                    let child_weight: u32 = self.graph[*child].1;
-                    if child_weight == *invalid_weight {
-                        unbalanced_node = *child;
-                    }
-                }
-                Some((unbalanced_node, *invalid_weight, *expected_weight))
-            }
-        }
     }
 
     fn update_node_value(&mut self, node: NodeIndex, weight: u32) {
         let mut node = self.graph.node_weight_mut(node).unwrap();
-        node.1 = weight;
+        node.2 = weight;
     }
 
-    fn update_weights(&mut self, current_nodes: Vec<NodeIndex>) {
+    fn compute_children_weights(&mut self, current_nodes: Vec<NodeIndex>) {
         let mut parents_to_update: HashSet<NodeIndex> = HashSet::new();
         for current_node in current_nodes {
             let children_weight: u32 = self.children_weight(current_node);
-            let current_node_weight: u32 = self.graph[current_node].1;
-            self.update_node_value(current_node, current_node_weight + children_weight);
+            self.update_node_value(current_node, children_weight);
 
             let parent = self.parent(current_node);
             if parent.is_some() {
@@ -125,106 +130,79 @@ impl GraphForRecursion {
 
         if !parents_to_update.is_empty() {
             let parents_to_update: Vec<NodeIndex> = parents_to_update.into_iter().collect();
-            self.update_weights(parents_to_update);
+            self.compute_children_weights(parents_to_update);
         }
     }
 
-    fn find_unbalanced_node(&self, current_nodes: Vec<NodeIndex>) -> Option<(NodeIndex, u32, u32)> {
-        let mut parents_to_check: HashSet<NodeIndex> = HashSet::new();
-
-        for current_node in current_nodes {
-            if self.children_weigh_unbalanced(current_node).is_some() {
-                println!("exit");
-                return self.children_weigh_unbalanced(current_node);
-            } else {
-                let parent = self.parent(current_node);
-                if parent.is_some() {
-                    parents_to_check.insert(parent.unwrap());
-                }
-            }
+    fn find_unbalanced_node(&self, current_node: NodeIndex) -> Option<(NodeIndex, u32)> {
+        // finding the unbalanced side
+        let mut children_weight_frequency: HashMap<u32, usize> = HashMap::new();
+        let mut children_weight_per_node: HashMap<u32, Vec<NodeIndex>> = HashMap::new();
+        for child in &self.children(current_node) {
+            let child_weight = self.get(*child);
+            let child_weight = child_weight.1 + child_weight.2;
+            children_weight_frequency
+                .entry(child_weight)
+                .and_modify(|f| *f += 1)
+                .or_insert(1);
+            children_weight_per_node
+                .entry(child_weight)
+                .and_modify(|l| l.push(*child))
+                .or_insert_with(|| vec![*child]);
         }
 
-// println!("parents_to_check {:?}",parents_to_check);
+        let unbalanced_side: u32 = children_weight_frequency
+            .iter()
+            .find(|(_, f)| **f == 1)
+            .map(|(w, _)| *w)
+            .unwrap();
+        let correct_side: u32 = children_weight_frequency
+            .iter()
+            .find(|(_, f)| **f > 1)
+            .map(|(w, _)| *w)
+            .unwrap();
+        let weight_diff: i32 = unbalanced_side as i32 - correct_side as i32;
 
-        if !parents_to_check.is_empty() {
-            let parents_to_check: Vec<NodeIndex> = parents_to_check.into_iter().collect();
-            self.find_unbalanced_node(parents_to_check)
+        let unbalanced_side: NodeIndex = children_weight_per_node[&unbalanced_side][0];
+
+        // looking at the children of the unbalanced side
+        let mut children_weight_frequency: HashMap<u32, usize> = HashMap::new();
+        for child in &self.children(unbalanced_side) {
+            let child_weight = self.get(*child);
+            let child_weight = child_weight.1 + child_weight.2;
+            children_weight_frequency
+                .entry(child_weight)
+                .and_modify(|f| *f += 1)
+                .or_insert(1);
+        }
+        if children_weight_frequency.len() == 1 {
+            // balanced, so the borken node is unbalanced_side
+            let new_weight = (self.get(unbalanced_side).1 as i32 - weight_diff) as u32;
+            Some((unbalanced_side, new_weight))
         } else {
-            None
+            self.find_unbalanced_node(unbalanced_side)
         }
     }
 }
 
 pub fn answer1(input: &str) -> String {
     let lines = parse_input(&input);
-    let mut graph = Graph::<String, u32>::new();
+    let towers = Towers::new(&lines);
 
-    // add all the nodes
-    let mut nodes: HashMap<String, NodeIndex> = HashMap::new();
-    for line in &lines {
-        let name = &line.name;
-        let node = graph.add_node(name.clone());
-        nodes.insert(name.clone(), node);
-    }
-
-    // add all the edges
-    for line in &lines {
-        if line.children.is_some() {
-            for child in &line.children.clone().unwrap() {
-                graph.update_edge(nodes[&line.name], nodes[child], 0); //0: no edge weight
-            }
-        }
-    }
-
-    let source_node = graph.externals(Direction::Incoming).next();
-    graph[source_node.unwrap()].clone()
+    towers.get(towers.source()).0
 }
 
 pub fn answer2(input: &str) -> u32 {
     let lines = parse_input(&input);
-    let mut graph = Graph::<(String, u32), u32>::new();
-
-    // add all the nodes
-    let mut nodes: HashMap<String, NodeIndex> = HashMap::new();
-    let mut weights: HashMap<NodeIndex, u32> = HashMap::new();
-    for line in &lines {
-        let name = &line.name;
-        let node = graph.add_node((name.clone(), line.weight));
-        nodes.insert(name.clone(), node);
-        weights.insert(node, line.weight);
-    }
-
-    // add all the edges
-    for line in &lines {
-        if line.children.is_some() {
-            for child in &line.children.clone().unwrap() {
-                graph.update_edge(nodes[&line.name], nodes[child], 0); //0: no edge weight
-            }
-        }
-    }
-
-    let mut gfr = GraphForRecursion { graph };
+    let mut towers = Towers::new(&lines);
 
     // starting for the leaf nodes of the graph, update all the weights
     // a node weight will be it's weight + the sum of it's childrens
-    gfr.update_weights(gfr.leafs());
-    println!("graph weight updated {:?}", &gfr.graph);
+    towers.compute_children_weights(towers.leafs());
 
     // for all nodes of the graph starting from the leaf,
     // find the first node for which the children don't have the same weight
-    let unbalanced = gfr.find_unbalanced_node(gfr.leafs()).unwrap();
-    println!("unbalanced {:?}", unbalanced);
-    let (unbalanced_node, invalid_weight, expected_weight) =unbalanced;
-    let original_weight = weights[&unbalanced_node];
-
-    //println!("{:?}", gfr.find_unbalanced_node(gfr.leafs()));
-    println!("original_weight{:?}", original_weight);
-
-    if invalid_weight > expected_weight {
-        original_weight - (invalid_weight - expected_weight)
-    } else {
-        original_weight + (expected_weight - invalid_weight)
-    }
+    towers.find_unbalanced_node(towers.source()).unwrap().1
 }
 
 fn parse_input(input: &str) -> Vec<Line> {
@@ -313,6 +291,72 @@ cntj (57)"#,
     );
 
     assert_eq!(answer1(&input), "tknk");
+}
+
+#[test]
+fn test_compute_children_weight() {
+    let input = String::from(
+        r#"
+pbga (66)
+xhth (57)
+ebii (61)
+havc (66)
+ktlj (57)
+fwft (72) -> ktlj, cntj, xhth
+qoyq (66)
+padx (45) -> pbga, havc, qoyq
+tknk (41) -> ugml, padx, fwft
+jptl (61)
+ugml (68) -> gyxo, ebii, jptl
+gyxo (61)
+cntj (57)"#,
+    );
+    let lines = parse_input(&input);
+    let mut towers = Towers::new(&lines);
+
+    towers.compute_children_weights(towers.leafs());
+
+    assert_eq!(
+        towers.get(towers.leafs()[0]),
+        Node("pbga".to_string(), 66, 0)
+    );
+    assert_eq!(
+        towers.get(towers.leafs()[1]),
+        Node("xhth".to_string(), 57, 0)
+    );
+    assert_eq!(
+        towers.get(towers.leafs()[2]),
+        Node("ebii".to_string(), 61, 0)
+    );
+    assert_eq!(
+        towers.get(towers.leafs()[3]),
+        Node("havc".to_string(), 66, 0)
+    );
+    assert_eq!(
+        towers.get(towers.leafs()[4]),
+        Node("ktlj".to_string(), 57, 0)
+    );
+    assert_eq!(
+        towers.get(towers.leafs()[5]),
+        Node("qoyq".to_string(), 66, 0)
+    );
+    assert_eq!(
+        towers.get(towers.leafs()[6]),
+        Node("jptl".to_string(), 61, 0)
+    );
+    assert_eq!(
+        towers.get(towers.leafs()[7]),
+        Node("gyxo".to_string(), 61, 0)
+    );
+    assert_eq!(
+        towers.get(towers.leafs()[8]),
+        Node("cntj".to_string(), 57, 0)
+    );
+
+    assert_eq!(
+        towers.get(towers.source()),
+        Node("tknk".to_string(), 41, 737)
+    );
 }
 
 #[test]
