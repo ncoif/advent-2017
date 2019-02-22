7188a95491b24288f0ab5c5fb5e9fe9ffa1b7d2e
ncoif <github@ncoif.net>
2019-02-22 00:46:41 +0000

    day10 answer1 wip

--- src/day10.rs
+++ src/day10.rs
@@ -17,23 +17,75 @@ named!(
 
 #[derive(Debug, PartialEq)]
 struct Hash {
-    list: Vec<u32>,
+    list: Vec<(usize, usize)>, // (prev, next)
+    list_size: usize,
+    list_start: usize,
     current_position: usize,
     skip_size: usize,
 }
 
 impl Hash {
     fn next(&mut self, next_length: usize) {
-        let slice = &mut self.list[self.current_position..next_length];
-        slice.reverse();
+        dbg!("start_next");
+
+        let first = self.list[self.list_start].0;
+        let mut last = self.list[self.list_start].1;
+        for _ in 1..next_length {
+            last = self.list[last].1
+        }
+        dbg!((first, last));
+
+        // reverse the sub-list
+        let mut cur_idx = self.list_start;
+        for idx in 0..next_length {
+            let cur = self.list[cur_idx];
+            // dbg!(&cur);
+            // dbg!(&self.list);
+
+            if idx == 0 {
+                // if first element, also update the edge
+                self.list[cur_idx].1 = last;
+                self.list[cur_idx].0 = cur.1;
+                self.list[last].0 = cur_idx;
+                dbg!(("first", &cur, "cur_idx", cur_idx));
+            } else if idx == next_length - 1 {
+                // if last element, also update the edge
+                self.list[cur_idx].1 = cur.0;
+                self.list[cur_idx].0 = first;
+                self.list[first].1 = cur_idx;
+                self.list_start = cur_idx + self.skip_size;
+                dbg!(("last", &cur, "cur_idx", cur_idx));
+            } else {
+                self.list[cur_idx].1 = cur.0;
+                self.list[cur_idx].0 = cur.1;
+            }
+
+            cur_idx = cur.1;
+        }
+
+        // and adjust the counters
+        self.current_position = (self.current_position + next_length + self.skip_size) % self.list_size;
+        self.skip_size += 1;
     }
 }
 
-pub fn answer1(size: usize, input: &str) -> u32 {
+pub fn answer1(size: usize, input: &str) -> usize {
     let lengths = parse_input(&input);
 
+    let list = (0..size)
+        .map(|idx| {
+            let prev = (size + idx -1) % size;
+            let next = (idx + 1) % size;
+            (prev, next)
+        })
+        .collect();
+
+    dbg!(&list);
+
     let mut hash = Hash {
-        list: (0..=size).map(|idx| idx as u32).collect(),
+        list: list,
+        list_size: size,
+        list_start: 0,
         current_position: 0,
         skip_size: 0,
     };
@@ -42,7 +94,7 @@ pub fn answer1(size: usize, input: &str) -> u32 {
         hash.next(length);
     }
 
-    hash.list[0] * hash.list[1]
+    hash.list_start * hash.list[hash.list_start].1
 }
 
 fn parse_input(input: &str) -> Vec<usize> {
@@ -57,12 +109,16 @@ fn test_parse_input() {
 #[test]
 fn test_next() {
     let mut init = Hash {
-        list: vec![0, 1, 2, 3, 4],
+        list: vec![(4, 1), (0, 2), (1, 3), (2, 4), (3, 0)],
+        list_size: 5,
+        list_start: 0,
         current_position: 0,
         skip_size: 0,
     };
     let expected = Hash {
-        list: vec![2, 1, 0, 3, 4],
+        list: vec![(1, 3), (2, 0), (4, 1), (0, 4), (3, 2)],
+        list_size: 5,
+        list_start: 2,
         current_position: 3,
         skip_size: 1,
     };
@@ -71,15 +127,57 @@ fn test_next() {
     assert_eq!(init, expected);
 
     let expected = Hash {
-        list: vec![4, 3, 0, 1, 2],
+        list: vec![(3, 1), (0, 2), (1, 4), (4, 0), (2, 3)],
+        list_size: 5,
+        list_start: 4,
         current_position: 3,
         skip_size: 2,
     };
     init.next(4);
     assert_eq!(init, expected);
+
+    let expected = Hash {
+        list: vec![(3, 1), (0, 2), (1, 4), (4, 0), (2, 3)],
+        list_size: 5,
+        list_start: 3,
+        current_position: 1,
+        skip_size: 3,
+    };
+    init.next(1);
+    assert_eq!(init, expected);
+
+    let expected = Hash {
+        list: vec![(1,3),(2,0),(4,1),(0,4),(3,2)],
+        list_size: 5,
+        list_start: 3,
+        current_position: 1,
+        skip_size: 3,
+    };
+
+}
+
+#[test]
+fn test_next_length_list_size() {
+    let mut init = Hash {
+        list: vec![(3,1),(0,2),(1,4),(4,0),(2,3)],
+        list_size: 5,
+        list_start: 4,
+        current_position: 2,
+        skip_size: 3,
+    };
+    let expected = Hash {
+        list: vec![(1,3),(2,0),(4,1),(0,4),(3,2)],
+        list_size: 5,
+        list_start: 3,
+        current_position: 4,
+        skip_size: 4,
+    };
+
+    init.next(5);
+    assert_eq!(init, expected);
 }
 
 #[test]
 fn test_answer1() {
-    assert_eq!(answer1(5, &"3,4,1,5".to_string()), 3 * 4);
+    // assert_eq!(answer1(5, &"3,4,1,5".to_string()), 3 * 4);
 }
