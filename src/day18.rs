88e21664f4a6f53ef9bb1b0bf1f2041edeeedf0a
ncoif <github@ncoif.net>
2019-03-12 21:49:20 +0000

    day18: answer1

--- src/day18.rs
+++ src/day18.rs
@@ -1,4 +1,5 @@
 use crate::common;
+use std::collections::HashMap;
 use std::str::FromStr;
 
 pub fn title() -> &'static str {
@@ -16,7 +17,8 @@ impl FromStr for Register {
 
     fn from_str(s: &str) -> Result<Register, ()> {
         let val = i64::from_str_radix(s, 10);
-        return if val.is_ok() {
+
+        if val.is_ok() {
             Ok(Register {
                 var: None,
                 val: Some(val.unwrap()),
@@ -27,7 +29,27 @@ impl FromStr for Register {
                 var: Some(var),
                 val: None,
             })
-        };
+        }
+    }
+}
+
+impl Register {
+    fn get(&self, s: &State) -> i64 {
+        if self.val.is_some() {
+            self.val.unwrap()
+        } else {
+            s.regs[&self.var.unwrap()]
+        }
+    }
+
+    fn var(&self) -> char {
+        self.var.unwrap()
+    }
+
+    fn initialize(&self, regs: &mut HashMap<char, i64>) {
+        if self.var.is_some() {
+            regs.insert(self.var.unwrap(), 0);
+        }
     }
 }
 
@@ -46,7 +68,6 @@ impl FromStr for Instruction {
     type Err = ();
 
     fn from_str(s: &str) -> Result<Instruction, ()> {
-        dbg!(&s);
         let mut words = s.split(' ');
         match words.next() {
             Some("snd") => Ok(Instruction::Send(
@@ -80,11 +101,78 @@ impl FromStr for Instruction {
     }
 }
 
-pub fn answer1(input: &str) -> u32 {
-    let instructions = parse_input(input);
-    dbg!(&instructions);
+impl Instruction {
+    fn apply(&self, s: &mut State) -> Option<i64> {
+        match self {
+            Instruction::Send(rx) => {
+                s.last_freq = Some(rx.get(s));
+                s.cur += 1;
+                None
+            }
+            Instruction::Set(rx, ry) => {
+                s.regs.insert(rx.var(), ry.get(s));
+                s.cur += 1;
+                None
+            }
+            Instruction::Add(rx, ry) => {
+                s.regs.insert(rx.var(), rx.get(s) + ry.get(s));
+                s.cur += 1;
+                None
+            }
+            Instruction::Mul(rx, ry) => {
+                s.regs.insert(rx.var(), rx.get(s) * ry.get(s));
+                s.cur += 1;
+                None
+            }
+            Instruction::Mod(rx, ry) => {
+                s.regs.insert(rx.var(), rx.get(s) % ry.get(s));
+                s.cur += 1;
+                None
+            }
+            Instruction::Recover(rx) => {
+                s.cur += 1;
+                if rx.get(s) != 0 && s.last_freq.is_some() {
+                    Some(s.last_freq.unwrap())
+                } else {
+                    None
+                }
+            }
+            Instruction::Jump(rx, ry) => {
+                if rx.get(s) != 0 {
+                    s.cur += ry.get(s) as isize;
+                } else {
+                    s.cur += 1;
+                }
+                None
+            }
+        }
+    }
+}
 
-    0
+#[derive(Debug)]
+struct State {
+    last_freq: Option<i64>,
+    regs: HashMap<char, i64>,
+    cur: isize,
+}
+
+pub fn answer1(input: &str) -> i64 {
+    let instructions = parse_input(input);
+    let registers = initialize(&instructions);
+
+    let mut state = State {
+        last_freq: None,
+        regs: registers,
+        cur: 0,
+    };
+
+    let mut result = None;
+    while state.cur >= 0 && state.cur <= instructions.len() as isize && result.is_none() {
+        let instruction = &instructions[state.cur as usize];
+        result = instruction.apply(&mut state);
+    }
+
+    result.unwrap()
 }
 
 fn parse_input(input: &str) -> Vec<Instruction> {
@@ -95,6 +183,41 @@ fn parse_input(input: &str) -> Vec<Instruction> {
         .collect();
 
     instructions
+}
+
+fn initialize(instructions: &[Instruction]) -> HashMap<char, i64> {
+    let mut registers = HashMap::new();
+    for instruction in instructions {
+        match instruction {
+            Instruction::Send(rx) => {
+                rx.initialize(&mut registers);
+            }
+            Instruction::Set(rx, ry) => {
+                rx.initialize(&mut registers);
+                ry.initialize(&mut registers);
+            }
+            Instruction::Add(rx, ry) => {
+                rx.initialize(&mut registers);
+                ry.initialize(&mut registers);
+            }
+            Instruction::Mul(rx, ry) => {
+                rx.initialize(&mut registers);
+                ry.initialize(&mut registers);
+            }
+            Instruction::Mod(rx, ry) => {
+                rx.initialize(&mut registers);
+                ry.initialize(&mut registers);
+            }
+            Instruction::Recover(rx) => {
+                rx.initialize(&mut registers);
+            }
+            Instruction::Jump(rx, ry) => {
+                rx.initialize(&mut registers);
+                ry.initialize(&mut registers);
+            }
+        }
+    }
+    registers
 }
 
 #[test]
@@ -145,7 +268,6 @@ snd a
 set a 0
 rcv a
 jgz a -1
-jgz a p
 set a 1
 jgz a -2"#,
     );
